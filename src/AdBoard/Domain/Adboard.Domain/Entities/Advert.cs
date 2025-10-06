a196c5af378e4123c5bfbd33d0d3454a2ec440c8
Alexey Shatskikh <shtskkh@gmail.com>
2025-10-06 21:41:08 +0700

    [dev] Removed preview photo relationship

--- src/AdBoard/Domain/Adboard.Domain/Entities/Advert.cs
+++ src/AdBoard/Domain/Adboard.Domain/Entities/Advert.cs
@@ -7,16 +7,15 @@ public class Advert : IEntity<Guid>
     public Guid Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public Guid PreviewPhoto { get; set; }
     public double Price { get; set; }
     public DateTime CreatedAt { get; set; }
     
     // FKs
     public Guid UserId { get; set; }
-    public Guid PreviewPhotoId { get; set; }
 
     // Navigation Properties
     public User User { get; set; }
-    public AdvertPhoto PreviewPhoto { get; set; }
     public ICollection<AdvertPhoto> Photos { get; set; }
     public ICollection<Subcategory> Subcategories { get; set; }
     public ICollection<AdvertComment> AdvertComments { get; set; }
