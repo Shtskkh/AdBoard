54ec6711d5e7c74eb8534c9add6ded4d1cc7ccc8
Alexey Shatskikh <shtskkh@gmail.com>
2025-10-18 12:03:27 +0700

    [dev] SelectCategoryDto documentation

--- src/AdBoard/Contracts/Adboard.Contracts/Categories/SelectCategoryDto.cs
+++ src/AdBoard/Contracts/Adboard.Contracts/Categories/SelectCategoryDto.cs
@@ -1,7 +1,17 @@
 namespace Adboard.Contracts.Categories;
 
+/// <summary>
+/// Модель выбора категории и подкатегорий
+/// </summary>
 public class SelectCategoryDto
 {
+    /// <summary>
+    /// Id категории
+    /// </summary>
     public int CategoryId { get; set; }
+    
+    /// <summary>
+    /// Массив id подкатегорий
+    /// </summary>
     public IReadOnlyCollection<int> Subcategories { get; set; }
 }
