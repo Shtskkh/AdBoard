30e4f32d53475a2e4cda2b45cbb718ab6f320b2c
Alexey Shatskikh <shtskkh@gmail.com>
2025-10-18 13:01:14 +0700

    [dev] Added method to get photos by advert id

--- src/AdBoard/Application/Adboard.AppServices/Contexts/AdvertsPhotos/IAdvertPhotoRepository.cs
+++ src/AdBoard/Application/Adboard.AppServices/Contexts/AdvertsPhotos/IAdvertPhotoRepository.cs
@@ -1,10 +1,10 @@
-
-
 using Adboard.Contracts.AdvertsPhotos;
+using Adboard.Domain.Entities;
 
 namespace Adboard.AppServices.Contexts.AdvertsPhotos;
 
 public interface IAdvertPhotoRepository
 {
+    Task<IReadOnlyCollection<AdvertPhoto>> GetByAdvertIdAsync(Guid advertId);
     Task<IReadOnlyCollection<Guid>> AddAsync(IEnumerable<CreateAdvertPhotoDto> photos);
 }
--- src/AdBoard/Infrastructure/Adboard.Infrastructure.DataAccess/Context/AdvertsPhotos/Repositories/AdvertPhotoRepository.cs
+++ src/AdBoard/Infrastructure/Adboard.Infrastructure.DataAccess/Context/AdvertsPhotos/Repositories/AdvertPhotoRepository.cs
@@ -3,12 +3,23 @@ using Adboard.Contracts.AdvertsPhotos;
 using Adboard.Domain.Entities;
 using Adboard.Infrastructure.DataAccess.Repositories;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 
 namespace Adboard.Infrastructure.DataAccess.Context.AdvertsPhotos.Repositories;
 
 public class AdvertPhotoRepository
     (IRepository<AdvertPhoto, Guid, ApplicationDbContext> repository, IMapper mapper) : IAdvertPhotoRepository
 {
+    public async Task<IReadOnlyCollection<AdvertPhoto>> GetByAdvertIdAsync(Guid advertId)
+    {
+        var photos = await repository.GetAllAsync()
+            .Where(p => p.AdvertId == advertId)
+            .OrderBy(p => p.Order)
+            .ToListAsync();
+        
+        return photos;
+    }
+
     public async Task<IReadOnlyCollection<Guid>> AddAsync(IEnumerable<CreateAdvertPhotoDto> photosDto)
     {
         var photos = mapper.Map<IReadOnlyCollection<AdvertPhoto>>(photosDto);
