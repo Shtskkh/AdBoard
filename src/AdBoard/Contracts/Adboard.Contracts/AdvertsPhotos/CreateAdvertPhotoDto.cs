e7a82c4eeab8b4fe1157fbb15d912bcdbb7e6957
Alexey Shatskikh <shtskkh@gmail.com>
2025-10-18 16:07:57 +0700

    [fix] Added advert id to creation model

--- src/AdBoard/Contracts/Adboard.Contracts/AdvertsPhotos/CreateAdvertPhotoDto.cs
+++ src/AdBoard/Contracts/Adboard.Contracts/AdvertsPhotos/CreateAdvertPhotoDto.cs
@@ -6,5 +6,6 @@ public class CreateAdvertPhotoDto : IFile
 {
     public string Name { get; set; }
     public byte[] Content { get; set; }
+    public Guid AdvertId { get; set; }
     public int Order { get; set; }
 }
